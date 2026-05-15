ec44e36a59cee7f3b3671b0171d3a1002706f159
Simon <s.a.naumenko@students.psu.by>
2026-05-16 01:06:09 +0300

    fix CustomException for kafka

--- src/main/java/com/bankSpark/analyticsService/exception/KafkaCustomException.java
+++ src/main/java/com/bankSpark/analyticsService/exception/KafkaCustomException.java
@@ -5,9 +5,22 @@ public class KafkaCustomException extends RuntimeException {
     private String topic;
     private Object value;
 
+//    public KafkaCustomException(String topic, Object value) {
+//        String message = "KafkaException in topic: " + topic + " value: " + value;
+//        super(message);
+//    }
+
     public KafkaCustomException(String topic, Object value) {
-        String message = "KafkaException in topic: " + topic + " value: " + value;
-        super(message);
+        super(String.format("Kafka error in topic: %s, value: %s", topic, value));
+        this.topic = topic;
+        this.value = value;
     }
+
+    public KafkaCustomException(String topic, Object value, Throwable cause) {
+        super(String.format("Kafka error in topic: %s, value: %s", topic, value), cause);
+        this.topic = topic;
+        this.value = value;
+    }
+
 
 }
