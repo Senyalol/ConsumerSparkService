dae798b1ff8cf06e7c7b91f30fbea46c9a94ac2e
Simon <s.a.naumenko@students.psu.by>
2026-06-02 04:07:47 +0300

    fix invite token admin migration

--- src/main/resources/db/migration/V6__AddAdminToken.sql
+++ src/main/resources/db/migration/V6__AddAdminToken.sql
@@ -1,6 +1,6 @@
 INSERT INTO invite_tokens (token, role, used, expires_at, created_at)
 VALUES (
-           'ADMIN-1650E6D0',
+           'ANALYST-1650E6D0',
            'ADMIN',
            FALSE,
            CURRENT_TIMESTAMP + INTERVAL '365 days',
